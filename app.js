a290baef7c42fd9343c3da34b49d604f730421a3
Muhammad Hamza <muhammadhamza2965@gmail.com>
2020-11-21 12:06:20 +0500

    Annotation delete - update & create added

--- app.js
+++ app.js
@@ -1,15 +1,111 @@
 const express = require('express')
 const app = express()
-const port = 3000
-const path = require('path')
+const port = 3000;
+const axios = require('axios');
+const {
+  v4: uuidv4
+} = require('uuid');
+var cors = require('cors')
+const path = require('path');
+var data = {};
+var rows = [];
+var total = [];
+//enable cors
+app.use(cors())
 //serve static files
-  app.use(express.static( path.join(__dirname, './public') ))
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: true }))
-  
+app.use(express.static(path.join(__dirname, './public')))
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: true
+}))
+
 
 // All other routes should redirect to the index.html
-app.get('/', (req, res) => {res.sendFile( path.join(__dirname, './public/html/editor.html') )});
-  
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/html/editor.html'))
+});
+
+app.get('/api/read', (req, res) => {
+  res.json(data);
+})
+
+//Routes for annotations
+app.post('/api/annotations', (req, res) => {
+  console.log(req.params);
+  const {
+    qoute,
+    ranges,
+    text
+  } = req.body;
+  res.json({
+    "id": "aa2edd3d-d6ca-48ad-93f5-fe2ecde302f0",
+    "ranges": [{
+      "start": "/div[2]/div[1]/div[1]/div[1]",
+      "startOffset": 1,
+      "end": "/div[2]/div[1]/div[1]/div[2]",
+      "endOffset": 13
+    }],
+    "text": "sd"
+  });
+
+  data = {
+
+    "id": uuidv4(),
+    "quote": qoute,
+    ranges: ranges,
+    text: text
+
+  };
+  console.log('in /api/annotations');
+});
+
+//somehow need to figure out how to load all the annoations once file has been loaded
+app.get('/api/search', (req, res) => {
+  res.send({
+    "id": "aa2edd3d-d6ca-48ad-93f5-fe2ecde302f0",
+    "ranges": [{
+      "start": "/div[2]/div[1]/div[1]/div[1]",
+      "startOffset": 1,
+      "end": "/div[2]/div[1]/div[1]/div[2]",
+      "endOffset": 13
+    }],
+    "text": "sd"
+  });
+  console.log('in search');
+});
+
+//Update one annotation
+app.put('/api/annotations/:id', (req, res) => {
+  const {
+    qoute,
+    ranges,
+    text
+  } = req.body;
+  res.json({
+    "id": uuidv4(),
+    "quote": qoute,
+    ranges: ranges,
+    text: text
+  });
+  console.log('in put');
+})
+//Delete one annotation
+app.delete('/api/annotations/:id', (req, res) => {
+  const {
+    qoute,
+    ranges,
+    text
+  } = req.body;
+  res.json({
+    "id": uuidv4(),
+    "quote": qoute,
+    ranges: ranges,
+    text: text
+  });
+  console.log('in delte');
+})
+
+
+
 
 app.listen(port, () => console.log(`Example app listening on ${port} port!`))
--- public/js/editor.js
+++ public/js/editor.js
@@ -1,104 +1,129 @@
-//load the doc
-var currPage = 1; //Pages are 1-based not 0-based
-var numPages = 0;
-var thePDF = null;
-var url = undefined;
 //add event listener to listen for any file upload
 document.querySelector("#pdf-upload").addEventListener("change", function (e) {
-    //get the first ulpoaded file
-    var file = e.target.files[0]
-    if (file.type != "application/pdf") {
-        console.error(file.name, "is not a pdf file.")
-        $('.container').append(`<div class="alert alert-primary" role="alert">
+  //get the first ulpoaded file
+  var file = e.target.files[0]
+  if (file.type != "application/pdf") {
+    console.error(file.name, "is not a pdf file.")
+    $('.container').append(`<div class="alert alert-primary" role="alert">
         ${file.name}, is not a pdf file, Please re-load choose again!
       </div>`)
-        return
-    }
+    return
+  }
 
-    //load the filerLoader web api to read the file content stored on user computer
-    var fileReader = new FileReader();
+  //load the filerLoader web api to read the file content stored on user computer
+  var fileReader = new FileReader();
 
-    //as file reader is loaded get the file content as result object
-    fileReader.onload = function () {
-        var typedarray = new Uint8Array(this.result);
-        //save name/file content in global url var
-        url = typedarray;
-        //render the pdf
-        PDFJS.getDocument(url)
-            .then(function (pdf) {
+  //as file reader is loaded get the file content as result object
+  fileReader.onload = function () {
+    var typedarray = new Uint8Array(this.result);
+    //save name/file content in global url var
+    url = typedarray;
+    //render the pdf
+    PDFJS.getDocument(url)
+      .then(function (pdf) {
 
-                // Get div#container and cache it for later use
-                var container = document.getElementById("container");
+        // Get div#container and cache it for later use
+        var container = document.getElementById("container");
 
-                // Loop from 1 to total_number_of_pages in PDF document
-                for (var i = 1; i <= pdf.numPages; i++) {
+        // Loop from 1 to total_number_of_pages in PDF document
+        for (var i = 1; i <= pdf.numPages; i++) {
 
-                    // Get desired page
-                    pdf.getPage(i).then(function (page) {
+          // Get desired page
+          pdf.getPage(i).then(function (page) {
 
-                        var scale = 1.5;
-                        var viewport = page.getViewport(scale);
-                        var div = document.createElement("div");
+            var scale = 1.5;
+            var viewport = page.getViewport(scale);
+            var div = document.createElement("div");
 
-                        // Set id attribute with page-#{pdf_page_number} format
-                        div.setAttribute("id", "page-" + (page.pageIndex + 1));
+            // Set id attribute with page-#{pdf_page_number} format
+            div.setAttribute("id", "page-" + (page.pageIndex + 1));
 
-                        // This will keep positions of child elements as per our needs
-                        div.setAttribute("style", "position: relative");
+            // This will keep positions of child elements as per our needs
+            div.setAttribute("style", "position: relative");
 
-                        // Append div within div#container
-                        container.appendChild(div);
+            // Append div within div#container
+            container.appendChild(div);
 
-                        // Create a new Canvas element
-                        var canvas = document.createElement("canvas");
+            // Create a new Canvas element
+            var canvas = document.createElement("canvas");
 
-                        // Append Canvas within div#page-#{pdf_page_number}
-                        div.appendChild(canvas);
+            // Append Canvas within div#page-#{pdf_page_number}
+            div.appendChild(canvas);
 
-                        var context = canvas.getContext('2d');
-                        canvas.height = viewport.height;
-                        canvas.width = viewport.width;
+            var context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
 
-                        var renderContext = {
-                            canvasContext: context,
-                            viewport: viewport
-                        };
+            var renderContext = {
+              canvasContext: context,
+              viewport: viewport
+            };
 
-                        // Render PDF page
-                        page.render(renderContext)
-                            .then(function () {
-                                // Get text-fragments
-                                return page.getTextContent();
-                            })
-                            .then(function (textContent) {
-                                // Create div which will hold text-fragments
-                                var textLayerDiv = document.createElement("div");
+            // Render PDF page
+            page.render(renderContext)
+              .then(function () {
+                // Get text-fragments
+                return page.getTextContent();
+              })
+              .then(function (textContent) {
+                // Create div which will hold text-fragments
+                var textLayerDiv = document.createElement("div");
 
-                                // Set it's class to textLayer which have required CSS styles
-                                textLayerDiv.setAttribute("class", "textLayer");
+                // Set it's class to textLayer which have required CSS styles
+                textLayerDiv.setAttribute("class", "textLayer");
 
-                                // Append newly created div in `div#page-#{pdf_page_number}`
-                                div.appendChild(textLayerDiv);
+                // Append newly created div in `div#page-#{pdf_page_number}`
+                div.appendChild(textLayerDiv);
 
-                                // Create new instance of TextLayerBuilder class
-                                var textLayer = new TextLayerBuilder({
-                                    textLayerDiv: textLayerDiv,
-                                    pageIndex: page.pageIndex,
-                                    viewport: viewport
-                                });
+                // Create new instance of TextLayerBuilder class
+                var textLayer = new TextLayerBuilder({
+                  textLayerDiv: textLayerDiv,
+                  pageIndex: page.pageIndex,
+                  viewport: viewport
+                });
 
-                                // Set text-fragments
-                                textLayer.setTextContent(textContent);
+                // Set text-fragments
+                textLayer.setTextContent(textContent);
 
-                                // Render text-fragments
-                                textLayer.render();
-                            });
-                    });
-                }
-            });
+                // Render text-fragments
+                textLayer.render();
+              });
+          });
+        }
+        //Annotatorjs code
+        var content = $('.container').annotator();
+    content.annotator('addPlugin', 'Store', {
+      // The endpoint of the store on your server.
+      prefix: 'http://localhost:3000/api',
+
+      // Attach the uri of the current page to all annotations to allow search.
+      // Attach the uri of the current page to all annotations to allow search.
+      annotationData: {
+        'uri': 'http://localhost:3000'
+      },
+      urls: {
+        // These are the default URLs.
+        create:  '/annotations',
+        update:  '/annotations/:id',
+        destroy: '/annotations/:id',
+        search:  '/search'
+      },
+      // This will perform a "search" action when the plugin loads. Will
+      // request the last 20 annotations for the current url.
+      // eg. /store/endpoint/search?limit=20&uri=http://this/document/only
+      loadFromSearch: {
+        'limit': 20,
+        'uri': 'http://localhost:3000/api/search'
+      },
+      showViewPermissionsCheckbox:true
+    });
+    content.data('annotator')
+        //Annotator ends here
+      });
 
 
-    };
-    //read the file as a bufferarray
-    fileReader.readAsArrayBuffer(file);
-})
+
+  };
+  //read the file as a bufferarray
+  fileReader.readAsArrayBuffer(file);
+});
